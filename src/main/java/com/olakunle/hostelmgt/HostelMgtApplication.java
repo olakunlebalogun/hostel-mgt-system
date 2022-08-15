6b401f92b953aedcc52c38a4e8a38b8e781ea036
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-08-15 11:59:54 +0100

    started hostel management system, first commit...

---
+++ src/main/java/com/olakunle/hostelmgt/HostelMgtApplication.java
@@ -0,0 +1,13 @@
+package com.olakunle.hostelmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelMgtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olakunle/hostelmgt/HostelMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olakunle.hostelmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
